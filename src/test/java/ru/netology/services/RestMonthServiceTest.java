2eff042c078b956544478b22cd96ec29beefe5c6
Xenia <xzinnatullina@mail.ru>
2023-06-29 19:45:04 +0300

    scr.modified

--- src/test/java/ru/netology/services/RestMonthServiceTest.java
+++ src/test/java/ru/netology/services/RestMonthServiceTest.java
@@ -2,26 +2,31 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
 
 public class RestMonthServiceTest {
 
-    @Test
-    public void calculateForCaseOne(){
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Rest.csv")
+    public void calculateForCaseOne(int expected, int income,int expenses,int threshold) {
         RestMonthService service = new RestMonthService();
-        int expected =3;
 
-        int actual = service.calculate(10_000,3_000,20_000);
+        int actual = service.calculate(income,expenses,threshold);
 
-        Assertions.assertEquals(expected,actual);
-
+        Assertions.assertEquals(expected, actual);
     }
+       // @Test
 
-    @Test
-    public void calculateForCaseTwo(){
-        RestMonthService service = new RestMonthService();
-        int expected = 2;
-        int actual = service.calculate(100_000,60_000,150_000);
+      //  public void calculateForCaseTwo(){
+          //  RestMonthService service = new RestMonthService();
+         //   int expected =2;
 
-        Assertions.assertEquals(expected,actual);
-    }
+      //      int actual = service.calculate(100_000,60_000,150_000);
+
+        //    Assertions.assertEquals(expected,actual);
+   // }
+
 }
