52fe7fb0c3ae9e56145efd2a806875cbbf68d2ff
Xenia <xzinnatullina@mail.ru>
2023-07-03 13:35:01 +0300

    src.test.remode

--- src/test/java/ru/netology/services/RestMonthServiceTest.java
+++ src/test/java/ru/netology/services/RestMonthServiceTest.java
@@ -11,22 +11,22 @@ public class RestMonthServiceTest {
 
     @ParameterizedTest
     @CsvFileSource(files = "src/test/resources/Rest.csv")
-    public void calculateForCaseOne(int expected, int income,int expenses,int threshold) {
+    public void calculateForCaseOne(int expected, int income, int expenses, int threshold) {
         RestMonthService service = new RestMonthService();
 
-        int actual = service.calculate(income,expenses,threshold);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
-       // @Test
+    // @Test
 
-      //  public void calculateForCaseTwo(){
-          //  RestMonthService service = new RestMonthService();
-         //   int expected =2;
+    //  public void calculateForCaseTwo(){
+    //  RestMonthService service = new RestMonthService();
+    //   int expected =2;
 
-      //      int actual = service.calculate(100_000,60_000,150_000);
+    //      int actual = service.calculate(100_000,60_000,150_000);
 
-        //    Assertions.assertEquals(expected,actual);
-   // }
+    //    Assertions.assertEquals(expected,actual);
+    // }
 
 }
